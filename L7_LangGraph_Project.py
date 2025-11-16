8b7f1b07fa1f3a5732e709c00f8e1fa4d15f2f4e
Shantnu-singh <singhshantnu2001@gmail.com>
2025-11-16 11:59:21 +0530

    Added LangGraph Chatbot Project frontend

--- L7_LangGraph_Project.py
+++ L7_LangGraph_Project.py
@@ -1,18 +1,55 @@
 import streamlit as st
 from L7_LangGraph_Project_backend import chatbot_workflows , HumanMessage 
-st.title("Simple Chabot")
+import uuid
 
-# Utility Fucntion
+# Utility fucntions
+def generate_thread_id():
+    return uuid.uuid4()
+
+def start_new_chat():
+    new_thread_id = generate_thread_id()
+    
+    st.session_state['thread_id'] = new_thread_id
+    add_thread_id(st.session_state['thread_id'])    
+
+    st.session_state['msg_hist'] = []
+    
+def add_thread_id(thread_id):
+    if thread_id not in st.session_state['thread_ids']:
+        st.session_state['thread_ids'].append(thread_id)
+    
 
 # Streamlit session state is a Dict, after you press enter it stay that way
 if "msg_hist" not in st.session_state:
     st.session_state['msg_hist'] = []
 
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id()
+    
+if 'thread_ids' not in st.session_state:
+    st.session_state['thread_ids'] = []
+
+add_thread_id(st.session_state['thread_id'])
+
+    
+# Streamlit UI
+st.title("Simple Chabot")
+
+with st.sidebar:
+    st.header("LangGraph Chatbot")
+    if st.button("New Chat" , type= "primary"):
+        start_new_chat()
+    st.header("My Conversations")
+    
+    for thread_id in st.session_state['thread_ids']:
+        st.button(str(thread_id))
 
 
+# Hist of all the msg
 for msg in st.session_state['msg_hist']:
     with st.chat_message(msg['role']):
         st.text(msg['content'])
+        
 
 
 user_msg = st.chat_input("Type here....")
@@ -26,7 +63,7 @@ if user_msg:
     
     
     # Reply from LLM
-    config = {"configurable" : {"thread_id" : "1"}}
+    config = {"configurable" : {"thread_id" : st.session_state['thread_id']}}
 
     # Instead of invoke use "stream" for streaming
     stream_reponce = chatbot_workflows.stream({"msg" : HumanMessage(content = user_msg)} , config = config , stream_mode="messages")
